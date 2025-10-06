cc2b2abd0d59b21036f844665320b7f4e6eaee0e
Jonathon Hare <jsh2@soton.ac.uk>
2025-10-06 13:47:40 +0100

    updates

---
+++ launch-iridisx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -p l4,scavenger_l4,slurm_a100
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH -c 32
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=your_email@soton.ac.uk
+#SBATCH --time=00:4:00
+
+module load conda/python3
+conda activate my-pytorch-env
+
+python cifar.py
---
+++ simple-sbatch-example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH -p l4,scavenger_l4
+#SBATCH --mem=1G
+#SBATCH --nodes=1
+#SBATCH -c 1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=your_email@soton.ac.uk
+#SBATCH --time=00:00:10
+
+echo "$HOSTNAME says Hello World"
